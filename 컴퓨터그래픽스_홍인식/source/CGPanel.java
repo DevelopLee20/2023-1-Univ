b15579e2280b4a9045e32d7e656c02ebe7c8fa3e
LeeInGyu <developlee20@gmail.com>
2023-03-10 04:30:24 +0900

    2023-03-10 컴퓨터그래픽스 백업

--- 컴퓨터그래픽스_홍인식/source/CGPanel.java
+++ 컴퓨터그래픽스_홍인식/source/CGPanel.java
@@ -11,7 +11,6 @@ public class CGPanel extends JPanel {
     private final int sizeY = numY*pix; // 모눈종이 y축 크기
     private ArrayList<xypos> arr;
     private panel_graph gpanel;
-    float start_x, start_y, end_x, end_y;
 
     CGPanel() {
         setLayout(new FlowLayout());
@@ -21,7 +20,7 @@ public class CGPanel extends JPanel {
         arr = new ArrayList<>();
     }
 
-    //(x,y)좌표 점 클래스
+    // (x,y)좌표 점 클래스
     class xypos{
         int x, y;
         public xypos(int x, int y){
@@ -40,7 +39,7 @@ public class CGPanel extends JPanel {
             super.paintComponent(g);
             Graphics2D g2 = (Graphics2D)g;
             g2.translate(sizeX,sizeY);      // 원점을 (sizeX,sizeY)로 이동.
-            g2.setColor(Color.BLACK);
+            g2.setColor(Color.gray);
             //모눈종이 가로세로줄 그리기
             //좌표평면 그리기
             g2.setStroke(new BasicStroke(1));  //stroke : 도형 테두리 설정. 내부채우기설정은 setFill.
@@ -51,43 +50,20 @@ public class CGPanel extends JPanel {
                 g2.drawLine(i, -sizeY, i, sizeY);
             }
             // x,y축 그리기(굵은 선)
+            g2.setColor(Color.BLACK);
             g2.setStroke(new BasicStroke(2));
             g2.drawLine(-sizeX, 0, sizeX, 0);
             g2.drawLine(0, -sizeY, 0, sizeY);
             //그림 그리기
-            // for(xypos p : arr){
-            //     g2.fillRect(p.x*pix, -(p.y+1)*pix, pix, pix);   // 네모는 왼쪽위에서 아래로 그림
-            // }
-            float m = (end_y-start_y)/(end_x-start_x);
-
-            if(1>m){
-                for(int x=(int)start_x; x<(int)end_x; x++){
-                    if(start_y % 1 > 0.5){
-                        g2.fillRect((int)x*pix, -(int)(start_y+1)*pix, pix, pix);
-                    }
-                    else{
-                        g2.fillRect((int)x*pix, -(int)(start_y)*pix, pix, pix);
-                    }
-                    start_y = start_y + m;
-                }
+            for(xypos p : arr){
+                g2.fillRect(p.x*pix, -(p.y+1)*pix, pix, pix);
             }
-            else{
-                for(int y=(int)start_y; y<(int)start_y; y++){
-                    if(start_x % 1 > 0.5){
-                        g2.fillRect((int)start_x*pix+1, (int)y, pix, pix);
-                    }
-                    else{
-                        g2.fillRect((int)start_x*pix, (int)y, pix, pix);
-                    }
-                }
-            }
-
         }
     }
-
     class panel_control extends JPanel{
         JTextField x1, y1, x2, y2;
         JButton btn1, btnClear;
+        int int_x1, int_y1, int_x2, int_y2;
 
         panel_control(){
             setPreferredSize(new Dimension(300, 100));
@@ -109,23 +85,53 @@ public class CGPanel extends JPanel {
             add(btnClear);
 
             btn1.addActionListener(e -> {
-                // DDA_Alg
-                int DDA_x1 = Integer.parseInt(x1.getText());
-                int DDA_y1 = Integer.parseInt(y1.getText());
-                int DDA_x2 = Integer.parseInt(x2.getText());
-                int DDA_y2 = Integer.parseInt(y2.getText());
-                start_x = DDA_x1;
-                start_y = DDA_y1;
-                end_x = DDA_x2;
-                end_y = DDA_y2;
-                
+                int_x1 = Integer.parseInt(x1.getText());
+                int_y1 = Integer.parseInt(y1.getText());
+                int_x2 = Integer.parseInt(x2.getText());
+                int_y2 = Integer.parseInt(y2.getText());
+                new DDA_Alg(int_x1, int_y1, int_x2, int_y2);
+
                 gpanel.repaint();
-                //arr.clear();    //좌표리스트 초기화
             });
             btnClear.addActionListener(e -> {
-                arr.clear();    //좌표리스트 초기화
+                arr.clear();
                 gpanel.repaint();
             });
+        }
+
+        class DDA_Alg{
+            private int x1, y1, x2, y2;
+            private float m;
+            private boolean m_bool;
+
+            DDA_Alg(int x1, int y1, int x2, int y2){
+                this.x1 = x1;
+                this.y1 = y1;
+                this.x2 = x2;
+                this.y2 = y2;
+                m = (float)(y2-y1) / (float)(x2-x1);
+                m_bool = (1 > m);
+                xyget();
+            }
+
+            public int return_int(float value){
+                if(value % 1 > 0.5){
+                    return (int)value + 1;
+                }
+                else{
+                    return (int)value;
+                }
+            }
+
+            public void xyget(){
+                if(m_bool){
+                    float y = y1;
+                    for(int x=x1; x<=x2; x++){
+                        arr.add(0, new xypos(x, return_int(y)));
+                        y += m;
+                    }
+                }
+            }
         }
     }
 }
--- 컴퓨터그래픽스_홍인식/source/Computer_Graphics.java
+++ 컴퓨터그래픽스_홍인식/source/Computer_Graphics.java
@@ -1,70 +1,112 @@
-import java.awt.*;
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
-public class Computer_Graphics{
-    public static void main(String args[]){
-        new MyFrame();
+public class Computer_Graphics extends JFrame{
+
+    public Computer_Graphics() {
+        add(new CGPanel());
+        pack();
+        setTitle("CGBoard");
+        //setLayout(new FlowLayout());    //배치관리자 설정
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Computer_Graphics();
     }
 }
 
-// 화면 출력기
-class MyFrame extends JFrame{
-    int rect_size = 10;
-    int rect_count = 40;
-    int start_x = 20;
-    int start_y = 20;
-    int global_size = rect_size * rect_count; // 모눈종이 y좌표 아래로 밀어야함
-    Graphics g;
+class CGPanel extends JPanel {
 
-    public MyFrame(){
-        // this : JFrame : 출력된 화면
-        this.setTitle("Drawing");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(1000,600);
-        this.setVisible(true);
+    private final int pix = 10;      // 픽셀 1개 가로세로 크기
+    private final int numX = 30;   // 제1사분면 x 좌표 범위
+    private final int numY = 30;   // 제1사분면 y 좌표 범위
+    private final int sizeX = numX*pix; // 모눈종이 x축 크기
+    private final int sizeY = numY*pix; // 모눈종이 y축 크기
+    private ArrayList<xypos> arr;
+    private panel_graph gpanel;
+
+    CGPanel() {
+        setLayout(new FlowLayout());
+        gpanel = new panel_graph();
+        add(gpanel);
+        add(new panel_control());
+        arr = new ArrayList<>();
     }
 
-    // getGraphics() 로 수정 필요
-    @Override
-    public void paint(Graphics g){
-        this.g = g;
-        paint_dot(this.g);
-        paint_Alg(this.g);
+    //(x,y)좌표 점 클래스
+    class xypos{
+        int x, y;
+        public xypos(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
     }
 
-    public int reverse_y(int y){
-        return global_size - y;
-    }
+    class panel_graph extends JPanel{
+        panel_graph(){
+            //모눈종이 끝 선 안잘리게 패널 크기를 모눈종이보다 조금 더 크게 한다.
+            setPreferredSize(new Dimension((sizeX+1)*2, (sizeY+1)*2));
+        }
 
-    // 모눈 종이 생성 : 실제 모눈 종이처럼 수정 필요(0,0의 격자 굵기는 조금 굵게)
-    public void paint_dot(Graphics g){
-        int paint_x = this.start_x;
-        int paint_y = this.start_y;
-
-        g.setColor(Color.gray);
-        for(int i=0; i<this.rect_count; i++){
-            for(int j=0; j<this.rect_count; j++){
-                g.drawRect(paint_x, reverse_y(paint_y), this.rect_size, this.rect_size);
-                paint_x += this.rect_size;
+        public void paintComponent(Graphics g){     // g는 GraphicsContext 객체
+            super.paintComponent(g);
+            Graphics2D g2 = (Graphics2D)g;
+            g2.translate(sizeX,sizeY);      // 원점을 (sizeX,sizeY)로 이동.
+            g2.setColor(Color.BLACK);
+            //모눈종이 가로세로줄 그리기
+            //좌표평면 그리기
+            g2.setStroke(new BasicStroke(1));  //stroke : 도형 테두리 설정. 내부채우기설정은 setFill.
+            for(int i=-sizeY; i<=sizeY; i+=pix){
+                g2.drawLine(-sizeX, i, sizeX, i);
             }
-            paint_x = this.start_x;
-            paint_y += this.rect_size;
+            for(int i=-sizeX; i<=sizeX; i+=pix){
+                g2.drawLine(i, -sizeY, i, sizeY);
+            }
+            // x,y축 그리기(굵은 선)
+            g2.setStroke(new BasicStroke(2));
+            g2.drawLine(-sizeX, 0, sizeX, 0);
+            g2.drawLine(0, -sizeY, 0, sizeY);
+            //그림 그리기
+            for(xypos p : arr){
+                g2.fillRect(p.x*pix, -(p.y+1)*pix, pix, pix);   // 네모는 왼쪽위에서 아래로 그림
+            }
         }
     }
+    class panel_control extends JPanel{
+        JTextField x1, y1, x2, y2;
+        JButton btn1, btnClear;
 
-    // 테스트 버전
-    public void paint_Alg(Graphics g){
-        g.clearRect(0, 0, 1000, 600);
-        paint_dot(g);
+        panel_control(){
+            setPreferredSize(new Dimension(300, 100));
+            x1 = new JTextField();  //좌표값 입력 텍스트필드
+            y1 = new JTextField();
+            x2 = new JTextField();
+            y2 = new JTextField();
+            btn1 = new JButton("DDA");
+            btnClear = new JButton("Clear");
 
-        g.setColor(Color.black);
-        for(int i=0; i<3; i++){
-            g.fillRect(30, reverse_y(30), this.rect_size, this.rect_size);
+            setLayout(new GridLayout(0, 4, 2, 2));    // 격자 형태 레이아웃
+            add(new JLabel("    X1   Y1"));
+            add(x1);
+            add(y1);
+            add(btn1);
+            add(new JLabel("    X2   Y2"));
+            add(x2);
+            add(y2);
+            add(btnClear);
+
+            btn1.addActionListener(e -> {
+	// 여기에다 DDA 알고리즘 호출
+                gpanel.repaint();
+                //arr.clear();    //좌표리스트 초기화
+            });
+            btnClear.addActionListener(e -> {
+                arr.clear();    //좌표리스트 초기화
+                gpanel.repaint();
+            });
         }
-    }
-
-    // DDA 알고리즘
-    public void DDA_Alg(Graphics g){
-
     }
 }
