7fb69ea05e5810b81681b4ffac55271b291e0f33
LeeInGyu <dldlsrb1414@gmail.com>
2023-03-28 19:35:45 +0900

    커밋 메시지 입력

--- 윈도우즈프로그래밍_남윤영/실행 폴더/Program.cs
+++ 윈도우즈프로그래밍_남윤영/실행 폴더/Program.cs
@@ -90,15 +90,15 @@
 //     Console.WriteLine();
 // }
 
-// 문제 5
-int num = 1;
+// // 문제 5
+// int num = 1;
 
-for(int i=0; i<=3; i++){
-    for(int q=1; q<=9; q++){
-        for(int p=num; p<=num+i; p++){
-            Console.Write(p + " * " + q + " = " + (p * q).ToString().PadRight(6, ' '));
-        }
-    Console.WriteLine();
-    }
-    num = num + i + 1;
-}
+// for(int i=0; i<=3; i++){
+//     for(int q=1; q<=9; q++){
+//         for(int p=num; p<=num+i; p++){
+//             Console.Write(p + " * " + q + " = " + (p * q).ToString().PadRight(6, ' '));
+//         }
+//     Console.WriteLine();
+//     }
+//     num = num + i + 1;
+// }
