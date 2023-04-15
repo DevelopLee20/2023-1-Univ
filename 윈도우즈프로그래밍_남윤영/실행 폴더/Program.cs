592b503e46ad85a745bc7e47f554fb048e9e80c2
LeeInGyu <developlee20@gmail.com>
2023-04-15 21:11:49 +0900

    2023-04-15 윈프 과제 #8

--- 윈도우즈프로그래밍_남윤영/실행 폴더/Program.cs
+++ 윈도우즈프로그래밍_남윤영/실행 폴더/Program.cs
@@ -1,307 +1,56 @@
-﻿// class ProtossUnit{
-//     public int strength = 40;
-//     public String skill = "클로킹";
-//     public int damage = 45;
-//     public int shield = 80;
-//     public String clan;
-//     public String name;
+﻿// // 다음 지시사항을 준수하여 프로그램을 작성하시오.
+// // (1) 오늘을 기준으로 근무시간을 출력하는 클래스 생성
+// // (2) 키보드로 입사일자 입력 (년-월-일 시:분)
+// // (3) DateTime 구조체와 TimeSpan 구조체 선언
+// // (4) 기타 사항은 실행 결과 참조
 
-//     public ProtossUnit(String clan, String name){
-//         this.clan = clan;
-//         this.name = name;
+// class Company{
+//     String InCompany;
+
+//     public Company(String InCompany){
+//         this.InCompany = InCompany;
 //     }
 
-//     public int getDamage(){
-//         return damage;
-//     }
+//     public void howLong(){
+//         DateTime NowTime = DateTime.Now;
+//         DateTime InTime = DateTime.Parse(InCompany);
 
-//     public int getStrength(){
-//         return strength;
-//     }
+//         TimeSpan GapTime = NowTime.Subtract(InTime);
 
-//     public int getShield(){
-//         return shield;
-//     }
+//         Console.WriteLine("> 입사일자 : " + InTime.ToString());
+//         Console.WriteLine("> 현재시간 : " + NowTime.ToString());
 
-//     public String getClan(){
-//         return clan;
-//     }
-
-//     public String getName(){
-//         return name;
-//     }
-
-//     public String getSkill(){
-//         return skill;
+//         Console.Write("> 지금까지 " + GapTime.Days + "일 ");
+//         Console.Write(GapTime.Hours + "시간 ");
+//         Console.Write(GapTime.Minutes + "분 ");
+//         Console.WriteLine(GapTime.Seconds + "초를 근무하였습니다.");
 //     }
 // }
 
-// class DarkTemplar : ProtossUnit{
-//     public DarkTemplar(String clan, String name) : base(clan, name){
+// class Program{
+//     public static void Main(String[] args){
+//         Console.Write("> 입사일자 입력(년-월-일 시:분) : ");
+//         String InCompany = Console.ReadLine();
+
+//         Company com = new Company(InCompany);
+//         com.howLong();
 //     }
 // }
 
-// class Nexus{
-//     int mineral;
-//     int gas;
-//     public Nexus(int mineral){
-//         this.mineral = mineral;
+// 02 다음 지시사항을 준수하여 프로그램을 작성하시오.
+// (1) 현재 날짜와 시간 기준 : 포맷 형식으로 출력하는 클래스 생성
+// (2) CultureInfo 클래스를 사용
+// (3) namespace 선언
+// (4) 기타 사항은 실행 결과 참조
 
-//         if(this.mineral >= 50){
-//             Console.WriteLine("프로브 생산");
-//         }
-//         else{
-//             Console.WriteLine("광물이 부족합니다.");
-//         }
-//     }
-
-//     public Nexus(int mineral, int gas){
-//         this.mineral = mineral;
-//         this.gas = gas;
-
-//         if(this.mineral >= 300 && this.gas >= 300){
-//             Console.WriteLine("모선 생산");
-//         }
-//         else{
-//             Console.WriteLine("광물 혹은 가스가 부족합니다.");
-//         }
-//     }
-// }
-
-// class program{
-//     static void Main(String[] args){
-//         ProtossUnit DT = new DarkTemplar("프로토스", "다크템플러");
-
-//         int Damage = DT.getDamage();
-//         int Strength = DT.getStrength();
-//         int Shield = DT.getShield();
-
-//         Console.WriteLine("종족: " + DT.getClan());
-//         Console.WriteLine("이름: " + DT.getName());
-//         Console.WriteLine("쉴드: " + Shield);
-//         Console.WriteLine("체력: " + Strength);
-//         Console.WriteLine("데미지: " + Damage.ToString());
-//         Console.WriteLine("스킬: " + DT.getSkill());
-//         Console.WriteLine();
-
-//         Nexus N1 = new Nexus(40);
-//         Nexus M1 = new Nexus(30, 30);
-
-//         Console.WriteLine();
-
-//         Nexus N2 = new Nexus(50);
-//         Nexus M2 = new Nexus(300, 300);
-
-//         Console.WriteLine();
-//     }
-// }
-
-// 2번
-
-abstract class Game{
-    public static int userWin=0, computerWin=0, drawMatch=0;
-
-    public Game(){
-    }
-
-    ~Game(){
-        recordPrint();
-    }
-
-    public abstract void result();
-
-    public void recordPrint(){
-        Console.WriteLine(userWin + "승 " + computerWin + "패 " + drawMatch + "무승부입니다.");
-    }
-}
-
-class Srp : Game{
-    public int shot;
-    public int user;
-
-    public Srp(){
-        Random rand = new Random();
-        this.shot = rand.Next(1,4);
-    }
-
-    public void play(){
-        Console.Write("입력하세요 [가위(1), 바위(2), 보(3), 종료(0)] : ");
-        int i = Convert.ToInt32(Console.ReadLine());
-
-        if(i == 0){
-            return;
-        }
-
-        this.user = i;
-
-        result();
-    }
-
-    public int check(){
-        if(user != 1 && shot > user){
-            return -1;  // 졌음
-        }
-        else if(shot == user){
-            return 0;   // 비겼음
-        }
-        else if(user == 1 && shot == 3){
-            return 1;   // 이겼음
-        }
-        else{
-            return 1;   // 이겼음
-        }
-    }
-
-    public override void result(){
-        int chk = check();
-        String print;
-
-        if(shot == 1){
-            print = "컴퓨터는 가위를 냈습니다.";
-        }
-        else if(shot == 2){
-            print = "컴퓨터는 바위를 냈습니다.";
-        }
-        else{
-            print = "컴퓨터는 보를 냈습니다.";
-        }
-
-        Console.WriteLine(print);
-
-        if(chk == 0){
-            print = "비겼습니다.";
-            drawMatch += 1;
-        }
-        else if(chk == -1){
-            print = "졌습니다.";
-            computerWin += 1;
-        }
-        else{
-            print = "이겼습니다.";
-            userWin += 1;
-        }
-        Console.WriteLine(print);
-        Console.WriteLine();
-    }
-}
-
-class Mjb : Srp{
-    int attacter = -1;
-    Random rand = new Random();
-
-    public Mjb(){
-        shot = rand.Next(1,4);
-    }
-
-    public void play(){
-        Console.Write("입력하세요 [가위(1), 바위(2), 보(3), 종료(0)] : ");
-        int i = Convert.ToInt32(Console.ReadLine());
-        shot = rand.Next(1,4);
-
-        if(i == 0){
-            return;
-        }
-        user = i;
-
-        result();
-    }
-
-    public override void result(){
-        int chk = check();
-        String print;
-
-        if(shot == 1){
-            print = "컴퓨터는 가위를 냈습니다.";
-        }
-        else if(shot == 2){
-            print = "컴퓨터는 바위를 냈습니다.";
-        }
-        else{
-            print = "컴퓨터는 보를 냈습니다.";
-        }
-
-        Console.WriteLine("check: " + chk);
-        Console.WriteLine(print);
-
-        if(attacter == -1){
-            if(chk == 0){
-                Console.WriteLine("비겼습니다.");
-                Console.WriteLine();
-                play();
-            }
-            else if(chk == -1){
-                Console.WriteLine("공격자는 컴퓨터입니다.");
-                Console.WriteLine();
-                attacter = 0;
-                play();
-            }
-            else{
-                Console.WriteLine("공격자는 유저입니다.");
-                Console.WriteLine();
-                attacter = 1;
-                play();
-            }            
-        }
-
-        else if(attacter == 1){
-            if(chk == 0){
-                Console.WriteLine("이겼습니다.");
-                Console.WriteLine();
-                userWin += 1;
-            }
-            else if(chk == -1){
-                Console.WriteLine("공격자는 컴퓨터입니다.");
-                Console.WriteLine();
-                attacter = 0;
-                play();
-            }
-            else{
-                Console.WriteLine("공격자는 유저입니다.");
-                Console.WriteLine();
-                play();
-            }
-        }
-
-        else{
-            if(chk == 0){
-                Console.WriteLine("졌습니다.");
-                Console.WriteLine();
-                computerWin += 1;
-            }
-            else if(chk == -1){
-                Console.WriteLine("공격자는 유저입니다.");
-                Console.WriteLine();
-                attacter = 1;
-                play();
-            }
-            else{
-                Console.WriteLine("공격자는 컴퓨터입니다.");
-                Console.WriteLine();
-                play();
-            }
-        }
-    }
-}
+using System.Globalization;
 
 class Program{
-    static void Main(String[] args){
-        while(true){
-            Console.Write("선택하세요 [가위바위보 게임(1), 묵찌빠 게임(2), 종료(0)] : ");
-            int i = Convert.ToInt32(Console.ReadLine());
-
-            if(i == 1){
-                new Srp().play();
-            }
-            else if(i == 2){
-                new Mjb().play();
-            }
-            else if(i == 0){
-                Console.WriteLine(1 + "승 " + 1 + "패 " + 0 + "무승부입니다.");
-                break;
-            }
-            else{
-                Console.WriteLine("잘못 입력하셨습니다.");
-            }
-        }
+    public static void Main(String[] args){
+        DateTime Today = DateTime.Now;
+        CultureInfo Culture = CultureInfo.CreateSpecificCulture("ko-KR");
+        Console.WriteLine("오늘 : " + Today.ToString("yyyy년 MM월 dd일", Culture));
+        Console.WriteLine("F : " + Today.ToString("F"));
+        Console.WriteLine("s : " + Today.ToString("s"));
     }
 }
